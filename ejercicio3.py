e8ccd8b3ee628db1c51d23f90271cba9bfe40cd7
Elena Benito del Pino <elena.bnt5@gmail.com>
2018-02-25 15:27:39 +0100

    ejercicio traductor de introducción a diccionarios

---
+++ ejercicio3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+translator = {'hello': 'hola', 
+			  'bye': 'adios', 
+			  'apple': 'manzana',  
+			  'red': 'rojo', 
+			  'world': 'mundo'}
+for key, value in translator.items():
+	print(key, '-->', value)
+
